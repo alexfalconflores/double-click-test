ac2dc900547cc491aa9cb5fff147515e44d0999d
Alex Stefano Falcon Flores <alexfalconflores@hotmail.com>
2024-09-05 03:23:58 -0500

    [ADD] ImageIconExtension

---
+++ Double Click Test/Extensions/ImageIconExtension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using System;
+using Windows.UI.Xaml.Markup;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace Double_Click_Test.Extensions;
+
+[MarkupExtensionReturnType(ReturnType = typeof(ImageIcon))]
+public sealed partial class ImageIconExtension : MarkupExtension
+{
+    public Uri? Source { get; set; }
+    protected override object ProvideValue()
+    {
+        return new ImageIcon
+        {
+            Source = new BitmapImage(Source)
+        };
+    }
+}
